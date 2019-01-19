6e08f6b59a4a48326bf38f1dfc30eb0c7014685d
crearo <rishabh.bhardwaj@tonboimaging.com>
2019-01-20 02:45:16 +0530

    Launch activity properly

--- app/src/main/java/com/crearo/launched/MainActivity.java
+++ app/src/main/java/com/crearo/launched/MainActivity.java
@@ -1,6 +1,10 @@
 package com.crearo.launched;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -8,6 +12,7 @@ import com.stfalcon.chatkit.messages.MessageInput;
 import com.stfalcon.chatkit.messages.MessagesList;
 import com.stfalcon.chatkit.messages.MessagesListAdapter;
 
+import java.util.List;
 import java.util.UUID;
 
 import butterknife.BindView;
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements
     private MessagesListAdapter<Message> messagesAdapter;
     private User user;
     private User phone;
+    private List<ApplicationInfo> packageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements
 
         ButterKnife.bind(this);
 
-        messagesAdapter = new MessagesListAdapter<>("senderId0", null);
+        messagesAdapter = new MessagesListAdapter<>("user", null);
         messagesAdapter.enableSelectionMode(this);
         messagesAdapter.setLoadMoreListener(this);
         messagesList.setAdapter(messagesAdapter);
@@ -59,8 +65,46 @@ public class MainActivity extends AppCompatActivity implements
                 getUser(),
                 input.toString());
         messagesAdapter.addToStart(message, true);
+        String packageName = getPackageNameFromName(input.toString());
+        if (packageName != null) {
+            Intent launchIntent = getPackageManager().getLaunchIntentForPackage(packageName);
+            if (launchIntent != null) {
+                messagesAdapter.addToStart(new Message("android", getPhone(), "Launching!"), false);
+                startActivity(launchIntent);
+            } else {
+                messagesAdapter.addToStart(new Message("android", getPhone(), "error lol"), false);
+            }
+        } else {
+            messagesAdapter.addToStart(new Message("android", getPhone(), "I kinda dumb bro"), false);
+        }
         return true;
     }
+
+    @Nullable
+    private String getPackageNameFromName(String msg) {
+        String fromClosestPackageName = null;
+        String fromClosestName = null;
+        for (ApplicationInfo pkg : getAppsList()) {
+            if (pkg.name != null && pkg.name.toLowerCase().equals(msg.toLowerCase())) {
+                return pkg.name;
+            }
+            if (pkg.name != null && pkg.name.toLowerCase().contains(msg.toLowerCase())) {
+                fromClosestName = pkg.packageName;
+            }
+            if (pkg.packageName != null && pkg.packageName.toLowerCase().contains(msg.toLowerCase())) {
+                fromClosestPackageName = pkg.packageName;
+            }
+        }
+        return fromClosestName != null ? fromClosestName : fromClosestPackageName;
+    }
+
+    public List<ApplicationInfo> getAppsList() {
+        if (packageList == null) {
+            packageList = getPackageManager().getInstalledApplications(PackageManager.GET_META_DATA);
+        }
+        return packageList;
+    }
+
 
     private User getUser() {
         if (user == null) {
