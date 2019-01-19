b3cc3a719582dfe907606c40646df3f32b785896
crearo <rishabh.bhardwaj@tonboimaging.com>
2019-01-19 20:10:40 +0530

    Init project

---
+++ app/src/main/java/com/crearo/launched/MainActivity.java
@@ -0,0 +1,27 @@
+package com.crearo.launched;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.stfalcon.chatkit.messages.MessageInput;
+import com.stfalcon.chatkit.messages.MessagesList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.input)
+    MessageInput messageInput;
+    @BindView(R.id.messagesList)
+    MessagesList messagesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        
+    }
+}
