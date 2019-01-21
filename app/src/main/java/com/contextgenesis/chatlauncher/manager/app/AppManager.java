633174f346e0886c85ba48b456088ee2189992e6
crearo <rishabh.bhardwaj@tonboimaging.com>
2019-01-21 20:45:57 +0530

    Use a singleton rootprovider until we don't have dagger in place

--- app/src/main/java/com/contextgenesis/chatlauncher/manager/app/AppManager.java
+++ app/src/main/java/com/contextgenesis/chatlauncher/manager/app/AppManager.java
@@ -9,30 +9,47 @@ import android.content.pm.ResolveInfo;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.Nullable;
+
 public class AppManager {
 
     private final PackageManager packageManager;
+    private List<AppInfo> appList;
 
     public AppManager(PackageManager packageManager) {
         this.packageManager = packageManager;
+        appList = getAppList();
     }
 
-    public List<AppInfo> getAppList() {
-        List<AppInfo> appList = new ArrayList<>();
+    public AppInfo getAppInfoFromName(String name) {
+        for (AppInfo appInfo : appList) {
+            if (appInfo.getLabel().equalsIgnoreCase(name)) {
+                return appInfo;
+            }
+        }
+        return null;
+    }
 
-        Intent i = new Intent(Intent.ACTION_MAIN);
-        i.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> resolveInfoList = packageManager.queryIntentActivities(i, 0);
-        for (ResolveInfo resolveInfo : resolveInfoList) {
-            appList.add(new AppInfo(resolveInfo.activityInfo.packageName,
-                    resolveInfo.activityInfo.name,
-                    resolveInfo.loadLabel(packageManager).toString()));
+    private List<AppInfo> getAppList() {
+        if (appList == null) {
+            appList = new ArrayList<>();
+            Intent i = new Intent(Intent.ACTION_MAIN);
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            List<ResolveInfo> resolveInfoList = packageManager.queryIntentActivities(i, 0);
+            for (ResolveInfo resolveInfo : resolveInfoList) {
+                appList.add(new AppInfo(resolveInfo.activityInfo.packageName,
+                        resolveInfo.activityInfo.name,
+                        resolveInfo.loadLabel(packageManager).toString()));
+            }
         }
 
         return appList;
     }
 
-    public boolean launchApp(Context context, AppInfo appInfo) {
+    public boolean launchApp(Context context, @Nullable AppInfo appInfo) {
+        if (appInfo == null) {
+            return false;
+        }
         Intent launchIntent = packageManager.getLaunchIntentForPackage(appInfo.componentName.getPackageName());
         if (launchIntent == null) {
             return false;
