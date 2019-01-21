0d51001a95883f8324082727a86bf5b71d2b31ab
crearo <rishabh.bhardwaj@tonboimaging.com>
2019-01-21 17:43:36 +0530

    Add AppManager and AppInfo for launching apps

---
+++ app/src/main/java/com/contextgenesis/chatlauncher/manager/app/AppManager.java
@@ -0,0 +1,48 @@
+package com.contextgenesis.chatlauncher.manager.app;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppManager {
+
+    private final PackageManager packageManager;
+
+    public AppManager(PackageManager packageManager) {
+        this.packageManager = packageManager;
+    }
+
+    private List<AppInfo> getAppList(PackageManager manager) {
+        List<AppInfo> appList = new ArrayList<>();
+
+        Intent i = new Intent(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> resolveInfoList = manager.queryIntentActivities(i, 0);
+        for (ResolveInfo resolveInfo : resolveInfoList) {
+            appList.add(new AppInfo(resolveInfo.activityInfo.packageName,
+                    resolveInfo.activityInfo.name,
+                    resolveInfo.loadLabel(manager).toString()));
+        }
+
+        return appList;
+    }
+
+    public boolean launchApp(Context context, AppInfo appInfo) {
+        Intent launchIntent = packageManager.getLaunchIntentForPackage(appInfo.componentName.getPackageName());
+        if (launchIntent == null) {
+            return false;
+        }
+        try {
+            context.startActivity(launchIntent);
+            return true;
+        } catch (ActivityNotFoundException e) {
+            return false;
+        }
+    }
+
+}
