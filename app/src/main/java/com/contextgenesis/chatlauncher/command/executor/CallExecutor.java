ab79a5a46c8a6aee9baf4c43b279eb72684a6f17
crearo <rishabh.bhardwaj@tonboimaging.com>
2019-01-26 03:40:23 +0530

    Continue calling in the first go after asking permissions
    Wait until we receive a message, and only then proceed. This works
    because we're working in a background thread

--- app/src/main/java/com/contextgenesis/chatlauncher/command/executor/CallExecutor.java
+++ app/src/main/java/com/contextgenesis/chatlauncher/command/executor/CallExecutor.java
@@ -8,6 +8,9 @@ import com.contextgenesis.chatlauncher.rx.RxBus;
 
 import javax.inject.Inject;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+
 import static com.contextgenesis.chatlauncher.events.PermissionsEvent.Type.REQUEST;
 
 public class CallExecutor extends CommandExecutor {
@@ -30,7 +33,16 @@ public class CallExecutor extends CommandExecutor {
         String input = (String) inputMessage.getArgs()[0];
 
         if (!callManager.isCallPermissionGranted()) {
-            rxBus.post(new PermissionsEvent(Manifest.permission.CALL_PHONE, REQUEST));
+            // send an event and wait until we receive a granted/denied event
+            PermissionsEvent permissionsEvent = Observable.create((ObservableOnSubscribe<PermissionsEvent>) emitter -> {
+                rxBus.register(PermissionsEvent.class)
+                        .filter(event -> event.getPermissions().equals(Manifest.permission.CALL_PHONE))
+                        .filter(event -> event.getType() != REQUEST)
+                        .subscribe(emitter::onNext);
+                rxBus.post(new PermissionsEvent(Manifest.permission.CALL_PHONE, REQUEST));
+            })
+                    .blockingFirst();
+            postOutput("Permission Status: " + permissionsEvent.getType());
         }
 
         if (callManager.call(input)) {
